c06d1844383911951ed1c51f841103e44cf72d6a
Prit Sapariya <61084180+PritSapariya@users.noreply.github.com>
2022-11-17 12:01:19 +0530

    Added form validation at cilent side

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -1,70 +1,94 @@
+import * as yup from 'yup';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 import {
   Button,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input,
   InputGroup,
   InputRightElement,
+  useBoolean,
   VStack,
 } from '@chakra-ui/react';
-import { useState } from 'react';
+import {
+  MUST_BE_EMAIL,
+  PASSWORD_VALIDATION,
+  REQUIRED_FIELD,
+} from '../../utilities/messages';
 
 const Login = () => {
-  const [show, setShow] = useState(false);
-  const handleClick = () => setShow(!show);
+  const [show, setShow] = useBoolean();
 
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const loginSchema = yup.object().shape({
+    email: yup.string().email(MUST_BE_EMAIL).required(REQUIRED_FIELD),
+    password: yup
+      .string()
+      .matches(
+        /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+        PASSWORD_VALIDATION
+      ),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+  } = useForm({ resolver: yupResolver(loginSchema) });
 
   const submitHandler = () => {};
 
   return (
-    <VStack spacing="10px">
-      <FormControl id="email-login" isRequired>
-        <FormLabel>Email Address</FormLabel>
-        <Input
-          value={email}
-          type="email"
-          placeholder="Enter Your Email Address"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </FormControl>
-      <FormControl id="password-login" isRequired>
-        <FormLabel>Password</FormLabel>
-        <InputGroup size="md">
+    <form onSubmit={handleSubmit(submitHandler)}>
+      <VStack spacing="10px">
+        <FormControl id="email-login" isInvalid={errors.email}>
+          <FormLabel>Email Address</FormLabel>
           <Input
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            type={show ? 'text' : 'password'}
-            placeholder="Enter password"
+            {...register('email')}
+            type="email"
+            placeholder="Enter Your Email Address"
           />
-          <InputRightElement width="4.5rem">
-            <Button h="1.75rem" size="sm" onClick={handleClick}>
-              {show ? 'Hide' : 'Show'}
-            </Button>
-          </InputRightElement>
-        </InputGroup>
-      </FormControl>
-      <Button
-        colorScheme="blue"
-        width="100%"
-        style={{ marginTop: 15 }}
-        onClick={submitHandler}
-      >
-        Login
-      </Button>
-      <Button
-        variant="solid"
-        colorScheme="red"
-        width="100%"
-        onClick={() => {
-          setEmail('guest@example.com');
-          setPassword('123456');
-        }}
-      >
-        Get Guest User Credentials
-      </Button>
-    </VStack>
+          <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
+        </FormControl>
+        <FormControl id="password-login" isInvalid={errors.password}>
+          <FormLabel>Password</FormLabel>
+          <InputGroup size="md">
+            <Input
+              {...register('password')}
+              type={show ? 'text' : 'password'}
+              placeholder="Enter password"
+            />
+            <InputRightElement width="4.5rem">
+              <Button h="1.75rem" size="sm" onClick={setShow.toggle}>
+                {show ? 'Hide' : 'Show'}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+          <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
+        </FormControl>
+        <Button
+          type="submit"
+          colorScheme="blue"
+          width="100%"
+          style={{ marginTop: 15 }}
+        >
+          Login
+        </Button>
+        <Button
+          variant="solid"
+          colorScheme="red"
+          width="100%"
+          onClick={() => {
+            setValue('email', 'guest@example.com');
+            setValue('password', '123456');
+          }}
+        >
+          Get Guest User Credentials
+        </Button>
+      </VStack>
+    </form>
   );
 };
 
--- frontend/src/components/Authentication/Signup.js
+++ frontend/src/components/Authentication/Signup.js
@@ -1,149 +1,152 @@
 import {
   Button,
   FormControl,
-  FormHelperText,
+  FormErrorMessage,
   FormLabel,
   Input,
   InputGroup,
   InputRightElement,
-  useToast,
+  useBoolean,
   VStack,
 } from '@chakra-ui/react';
-import { getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage';
-import { useState } from 'react';
-import { storage } from '../../config/firebaseConfig';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import {
+  CONFIRM_PASSWORD_VALIDATION,
+  MUST_BE_CHARACTER,
+  MUST_BE_EMAIL,
+  MUST_BE_IMAGE,
+  PASSWORD_VALIDATION,
+  REQUIRED_FIELD,
+} from '../../utilities/messages';
+import { SUPPORTED_FILE_FORMATS } from '../../utilities/constants';
+import { uploadImageToStorage } from '../../utilities/helper';
+import useMessageToast from '../../hooks/useMessageToast';
 
 const Signup = () => {
-  const [show, setShow] = useState(false);
-  const handleClick = () => setShow(!show);
+  const [show, setShow] = useBoolean();
+  const { errorToast } = useMessageToast();
 
-  const [name, setName] = useState();
-  const [email, setEmail] = useState();
-  const [confirmpassword, setConfirmpassword] = useState();
-  const [password, setPassword] = useState();
-  const [pic, setPic] = useState();
-  const [progress, setProgress] = useState(0);
+  const signupSchema = yup.object().shape({
+    name: yup
+      .string()
+      .trim()
+      .matches(/^[a-zA-Z ]*$/, MUST_BE_CHARACTER)
+      .required(REQUIRED_FIELD),
+    email: yup.string().email(MUST_BE_EMAIL).required(REQUIRED_FIELD),
+    password: yup
+      .string()
+      .matches(
+        /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+        PASSWORD_VALIDATION
+      ),
+    confirmpassword: yup
+      .string()
+      .oneOf([yup.ref('password')], CONFIRM_PASSWORD_VALIDATION)
+      .required(REQUIRED_FIELD),
+    pic: yup
+      .mixed()
+      .test(
+        'fileType',
+        MUST_BE_IMAGE,
+        (values) =>
+          values.length > 0 && SUPPORTED_FILE_FORMATS.includes(values[0].type)
+      ),
+  });
 
-  const toast = useToast();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(signupSchema) });
 
-  const imageErrorToast = (title = 'Please select an Image!') =>
-    toast({
-      title: title,
-      status: 'warning',
-      position: 'bottom',
-      isClosable: true,
-      duration: 5000,
-    });
-
-  const postDetails = (pic) => {
+  const submitHandler = async (data) => {
     try {
-      if (pic === undefined) {
-        imageErrorToast();
-      }
-      if (pic.type === 'image/jpeg' || pic.type == 'image/png') {
-        // Uploading Image to firebase storage
-        const storageRef = ref(
-          storage,
-          `${process.env.REACT_APP_FIREBASE_STORAGE_FOLDER_URL}${pic.name}`
-        );
-        const uploadTask = uploadBytesResumable(storageRef, pic, {
-          contentType: pic.type,
-        });
-        uploadTask.on(
-          'state_changed',
-          (snapshot) => {
-            const prog =
-              (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            setProgress(prog);
-          },
-          (err) => console.error(err),
-          () => {
-            getDownloadURL(uploadTask.snapshot.ref).then((url) =>
-              // store this file URL
-              console.log(url)
-            );
-          }
-        );
-      } else {
-        imageErrorToast();
-        return;
-      }
+      const fileURL = await uploadImageToStorage(data.pic[0]);
+      console.log(fileURL);
     } catch (error) {
-      imageErrorToast('Network Error');
-    } finally {
-      return;
+      errorToast('Network Error');
     }
+    // handle the form submisssion logic
   };
-  const submitHandler = () => {};
 
   return (
-    <VStack spacing="5px">
-      <FormControl id="firstName" isRequired>
-        <FormLabel>Name</FormLabel>
-        <Input
-          placeholder="Enter Your Name"
-          onChange={(e) => setName(e.target.value)}
-        />
-      </FormControl>
-      <FormControl id="email-signup" isRequired>
-        <FormLabel>Email Address</FormLabel>
-        <Input
-          type="email"
-          placeholder="Enter Your Email Address"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </FormControl>
-      <FormControl id="password-signup" isRequired>
-        <FormLabel>Password</FormLabel>
-        <InputGroup size="md">
+    <form onSubmit={handleSubmit(submitHandler)}>
+      <VStack spacing="5px">
+        <FormControl id="firstName" isInvalid={errors.name}>
+          <FormLabel>Name</FormLabel>
           <Input
-            type={show ? 'text' : 'password'}
-            placeholder="Enter Password"
-            onChange={(e) => setPassword(e.target.value)}
+            type="text"
+            name="name"
+            {...register('name')}
+            placeholder="Enter Your Name"
           />
-          <InputRightElement width="4.5rem">
-            <Button h="1.75rem" size="sm" onClick={handleClick}>
-              {show ? 'Hide' : 'Show'}
-            </Button>
-          </InputRightElement>
-        </InputGroup>
-      </FormControl>
-      <FormControl id="confirmPassword" isRequired>
-        <FormLabel>Confirm Password</FormLabel>
-        <InputGroup size="md">
+          <FormErrorMessage>{errors.name?.message}</FormErrorMessage>
+        </FormControl>
+        <FormControl id="email-signup" isInvalid={errors.email}>
+          <FormLabel>Email Address</FormLabel>
           <Input
-            type={show ? 'text' : 'password'}
-            placeholder="Enter Confirm Password"
-            onChange={(e) => setConfirmpassword(e.target.value)}
+            type="email"
+            name="email"
+            {...register('email')}
+            placeholder="Enter Your Email Address"
           />
-          <InputRightElement width="4.5rem">
-            <Button h="1.75rem" size="sm" onClick={handleClick}>
-              {show ? 'Hide' : 'Show'}
-            </Button>
-          </InputRightElement>
-        </InputGroup>
-      </FormControl>
-      <FormControl id="pic">
-        <FormLabel>Upload your Picture</FormLabel>
-        <Input
-          type="file"
-          p={1.5}
-          accept="image/*"
-          onChange={(e) => postDetails(e.target.files[0])}
-        />
-        {progress > 0 && (
-          <FormHelperText>Uploaded... {progress}%</FormHelperText>
-        )}
-      </FormControl>
-      <Button
-        colorScheme="blue"
-        width="100%"
-        style={{ marginTop: 15 }}
-        onClick={submitHandler}
-      >
-        Sign Up
-      </Button>
-    </VStack>
+          <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
+        </FormControl>
+        <FormControl id="password-signup" isInvalid={errors.password}>
+          <FormLabel>Password</FormLabel>
+          <InputGroup size="md">
+            <Input
+              type={show ? 'text' : 'password'}
+              {...register('password')}
+              placeholder="Enter Password"
+            />
+            <InputRightElement width="4.5rem">
+              <Button h="1.75rem" size="sm" onClick={setShow.toggle}>
+                {show ? 'Hide' : 'Show'}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+          <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
+        </FormControl>
+        <FormControl id="confirmPassword" isInvalid={errors.confirmpassword}>
+          <FormLabel>Confirm Password</FormLabel>
+          <InputGroup size="md">
+            <Input
+              type={show ? 'text' : 'password'}
+              {...register('confirmpassword')}
+              placeholder="Enter Confirm Password"
+            />
+            <InputRightElement width="4.5rem">
+              <Button h="1.75rem" size="sm" onClick={setShow.toggle}>
+                {show ? 'Hide' : 'Show'}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+          <FormErrorMessage>{errors.confirmpassword?.message}</FormErrorMessage>
+        </FormControl>
+        <FormControl id="pic-signup" isInvalid={errors.pic}>
+          <FormLabel>Upload your Picture</FormLabel>
+          <Input
+            type="file"
+            name="pic"
+            p={1.5}
+            accept="image/*"
+            {...register('pic')}
+          />
+          <FormErrorMessage>{errors.pic?.message}</FormErrorMessage>
+        </FormControl>
+        <Button
+          type="submit"
+          colorScheme="blue"
+          width="100%"
+          style={{ marginTop: 15 }}
+        >
+          Sign Up
+        </Button>
+      </VStack>
+    </form>
   );
 };
 
